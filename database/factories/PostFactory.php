8b2f6de0194a7b0498d7ee91603d2a05556fafd4
Khomeini Khadafy <Khomeini,khadafy@gmail.com>
2025-01-02 02:06:06 +0700

    menambahkan namespace'

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use App\Models\User;
 use App\Models\Category;
 use Illuminate\Support\Str;
+use Database\Factories\PostFactory;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
