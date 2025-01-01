c32470e32862ae7460763a9743b359f93db8fd42
Khomeini Khadafy <Khomeini,khadafy@gmail.com>
2025-01-01 17:53:18 +0700

    menambahkan dibagian routes yaitu: untuk keseluruhan authors untuk menampilkan keseluruhan postingan yang telah dibuat

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 
-use Illuminate\Support\Facades\Route;
 use App\Models\Post;
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
@@ -21,6 +22,11 @@ Route::get('/posts', function () {
 Route::get('/posts/{post:slug}', function(Post $post) {
 
     return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+Route::get('/authors/{user}', function(User $user) {
+
+    return view('posts', ['title' => 'Article by ' . $user->name, 'posts' => $user->posts]);
 });
 
 Route::get('/contact', function () {
