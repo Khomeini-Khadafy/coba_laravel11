99a10671168c7f261ce39a4c09f86bfe724b1943
Khomeini Khadafy <Khomeini,khadafy@gmail.com>
2025-01-01 17:37:47 +0700

    menambahkan foreignKey didalam public function post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -11,7 +12,7 @@ class Post extends Model
     use HasFactory;
     protected $fillable = ['tilte', 'author', 'slug', 'body'];
 
-    // eloquent relationship "BelongsTo"
+    // eloquent relationship "BelongsTo" untuk author
     public function author(): BelongsTo
     {
         return $this->belongsTo(User::class);
