16288ae05aadd3215832e626b3fd5023481e7862
Khomeini Khadafy <Khomeini,khadafy@gmail.com>
2025-01-01 17:08:39 +0700

    menambahkan relationship ke models post biar terhubung ke database

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,11 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
     use HasFactory;
     protected $fillable = ['tilte', 'author', 'slug', 'body'];
+
+    // eloquent relationship "BelongsTo"
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
+
