6e1eebda06c2d6ebaa948a6603d34a83cb4dbf6d
Khomeini Khadafy <Khomeini,khadafy@gmail.com>
2025-01-04 01:30:09 +0700

    membuat file seeder baru (CategorySeeder) yang kedua membuat data manual category

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // contoh membuat data category manual
+        Category::create([
+            'name' => 'Web Design',
+            'slug' => 'web-design',
+        ]);
+        Category::create([
+            'name' => 'Manchine Learning',
+            'slug' => 'manchine-learning',
+        ]);
+        Category::create([
+            'name' => 'UI UX',
+            'slug' => 'ui-ux',
+        ]);
+        Category::create([
+            'name' => 'Data Structure',
+            'slug' => 'web-design',
+        ]);
+
+    }
+}
