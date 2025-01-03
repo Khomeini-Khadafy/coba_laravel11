bc77c3bc0afe52dafc38b3bc5330242c79c23f2f
Khomeini Khadafy <Khomeini,khadafy@gmail.com>
2025-01-04 00:52:11 +0700

    membuat data manual didalam UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,15 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // membuat manual data seeder
+        User::create([
+            'name'              => 'Khomeini Khadafy',
+            'username'          => 'khomeini.khadafy',
+            'email'             => 'khomeini.khadafy@gmail.com',
+            'email_verified_at' => 'now',
+            'remember_token'    => 'Str::random(10)'
+        ]);
+        
+        User::factory(5)->create();
     }
 }
