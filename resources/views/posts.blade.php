46a03087b5868d72d01a89ddcce1eb57de6f164d
Khomeini Khadafy <Khomeini,khadafy@gmail.com>
2025-01-08 21:41:22 +0700

    install package typography mengikuti perintah documentasi redesign ui template flowbite single page blog clear design

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,25 +4,6 @@
     <div class="py-4 px-4 mx-auto max-w-screen-xl lg:py-4 lg:px-0">
         <div class="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
             @foreach ($posts as $post)
-                {{-- <article class="py-8 max-w-screen-md border-b border-gray-300">
-              <a href="/posts/{{ $post['slug'] }}">
-                  <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-700">
-                      {{ $post['title'] }}
-                  </h2>
-              </a>
-              <di>
-                  <a class="text-base text-gray-500 hover:underline"
-                      href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a>
-                  <a class="text-base text-gray-500 hover:underline"
-                      href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a> |
-                  {{ $post->created_at->diffForHumans() }}
-              </di>
-              <p>
-                  {{ Str::limit($post['body']), 150 }}
-              </p>
-              <a href="/posts/{{ $post['slug'] }}">Read more &raquo;</a>
-          </article> --}}
-
                 <article
                     class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
                     <div class="flex justify-between items-center mb-5 text-gray-500">
@@ -41,26 +22,20 @@
                     </a>
                     <p class="mb-5 font-light text-gray-500 dark:text-gray-400">{{ Str::limit($post['body']), 50 }}</p>
                     <div class="flex justify-between items-center">
-                        <div class="flex items-center space-x-4">
+                        <div class="flex items-center space-x-2">
                             <img class="w-7 h-7 rounded-full"
                                 src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png"
                                 alt="Jese Leos avatar" />
                             <a href="/authors/{{ $post->author->username }}">
-                                <span class="font-medium dark:text-white">
+                                <span class="font-medium text-sm dark:text-white">
                                     {{ $post->author->name }}
                                 </span>
                             </a>
                         </div>
                         <a href="/posts/{{ $post->slug }}"
                             class="inline-flex items-center font-medium
-                           text-primary-600 dark:text-primary-500 hover:underline">
+                           text-primary-600 text-sm dark:text-primary-500">
                             Read more &raquo;
-                            <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
-                                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
                         </a>
                     </div>
                 </article>
