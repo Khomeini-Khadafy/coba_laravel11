4615cb0c05584a149b05a2d379bfe341dccef27b
Khomeini Khadafy <Khomeini,khadafy@gmail.com>
2025-01-12 08:12:09 +0700

    menambahkan 404 not found di posts ketika mencari judul di dalam category

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -71,9 +71,14 @@
                     </div>
                 </article>
             @empty
-                <div>
-                    <p class="font-semibold text-xl my-4">Article not found!</p>
-                    <a href="/posts" class="block text-blue-600">&laquo; Back to all posts</a>
+                <div class="justify-center items-center grid-cols-16">
+                    <p class="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl ">
+                        Something's missing.</p>
+                    <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">Sorry, we can't find that page.
+                        You'll find lots to explore on the all page. </p>
+                    <a href="/posts"
+                        class="inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4">Back
+                        to Posts</a>
                 </div>
             @endforelse
         </div>
